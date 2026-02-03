dd4834a5ada890ad0573071e451c4a2eb4892e6b
AliffIkhmal <aliffikhmal08@gmail.com>
2026-02-03 12:58:31 +0800

    first commit

---
+++ py/01_variables_data_types.py
@@ -0,0 +1,26 @@
+name = "Aliff"          # String variable
+age = 25                # Integer variable
+weight = 70.5           # Float variable
+is_female = False       # Boolean variable
+
+print(name)
+print(type(name))       # type() function to check data type for example string, integer, float, boolean
+print(type(age))
+print(type(weight))
+print(type(is_female))  
+
+# Math operations
+# a = 10
+# b = 3
+
+# print(a + b)    # Addition
+# print(a - b)    # Subtraction
+# print(a * b)    # Multiplication
+# print(a / b)    # Division
+# print(a // b)   # Floor Division
+# print(a % b)    # Modulus
+# print(a ** b)   # Exponentiation
+
+celsius = 25
+fahrenheit = (celsius * 9/5) + 32
+print(f"The temperature in Fahrenheit is: {fahrenheit}")  # Convert Celsius to Fahrenheit    
